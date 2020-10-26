43938258bb61afff0ade48a4ea79f9e128c485bc
Harman Sandhu <hrrsandhu6@gmail.com>
2020-10-25 22:02:44 -0700

    Initial Commit

---
+++ CameraDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CameraDemo
+//
+//  Created by Harry Sandhu on 2020-10-25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
